7b8df6b95fb06de3d2986fb8a427614c769d9b0f
elefher <eldelielev@gmail.com>
2014-12-24 05:41:26 +0200

    add libraries to support ads

--- src/com/cpu/handler/MainActivity.java
+++ src/com/cpu/handler/MainActivity.java
@@ -1,17 +1,8 @@
 package com.cpu.handler;
 
-import java.util.ArrayList;
-
-import com.elefher.customclasses.CpuGpuFreqVoltages;
-import com.elefher.customclasses.Gpu;
-import com.elefher.tab.ControlCpu;
-import com.elefher.tab.ControlGpu;
-import com.elefher.tab.Info;
-import com.elefher.tab.MiscTools;
-import com.elefher.tab.Voltages;
-
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,14 +10,19 @@ import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.TextView;
+import com.elefher.customclasses.CpuGpuFreqVoltages;
+import com.elefher.customclasses.Gpu;
+import com.elefher.tab.*;
+
+import java.util.ArrayList;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends Activity {
 
@@ -133,5 +129,86 @@ public class MainActivity extends Activity {
 		});
 
 		alert.show();
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view. This fragment
+	 * would include your content.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+			return rootView;
+		}
+	}
+
+	/**
+	 * This class makes the ad request and loads the ad.
+	 */
+	public static class AdFragment extends Fragment {
+
+		private AdView mAdView;
+
+		public AdFragment() {
+		}
+
+		@Override
+		public void onActivityCreated(Bundle bundle) {
+			super.onActivityCreated(bundle);
+
+			// Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
+			// values/strings.xml.
+			mAdView = (AdView) getView().findViewById(R.id.adView);
+
+			// Create an ad request. Check logcat output for the hashed device ID to
+			// get test ads on a physical device. e.g.
+			// "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+			AdRequest adRequest = new AdRequest.Builder()
+					.addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+					.build();
+
+			// Start loading the ad in the background.
+			mAdView.loadAd(adRequest);
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+								 Bundle savedInstanceState) {
+			return inflater.inflate(R.layout.fragment_ad, container, false);
+		}
+
+		/** Called when leaving the activity */
+		@Override
+		public void onPause() {
+			if (mAdView != null) {
+				mAdView.pause();
+			}
+			super.onPause();
+		}
+
+		/** Called when returning to the activity */
+		@Override
+		public void onResume() {
+			super.onResume();
+			if (mAdView != null) {
+				mAdView.resume();
+			}
+		}
+
+		/** Called before the activity is destroyed */
+		@Override
+		public void onDestroy() {
+			if (mAdView != null) {
+				mAdView.destroy();
+			}
+			super.onDestroy();
+		}
+
 	}
 }
