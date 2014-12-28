65a884f3f43151922b71464df3cbd48b329d8f6d
elefher <eldelielev@gmail.com>
2014-12-28 18:06:01 +0200

    cpuinfolayout

--- src/com/elefher/abstractclasses/CpuInfoLayout.java
+++ src/com/elefher/abstractclasses/CpuInfoLayout.java
@@ -1,16 +1,19 @@
 package com.elefher.abstractclasses;
 
-import android.app.Activity;
 import android.content.Context;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import com.elefher.utils.MiscProgressBar;
 
 /**
  * Created by elefher on 28/12/2014.
  */
-public abstract class CpuInfoLayout{
+public abstract class CpuInfoLayout {
     LinearLayout layout;
     TextView textView;
+    MiscProgressBar progressBar;
+
     public abstract void LayoutSettings(Context cntx);
     public abstract void TextViewSettings(Context cntx);
+    public abstract void progressBarSettings(Context cntx, int Rdrawable, LinearLayout.LayoutParams paramsCircle);
 }
